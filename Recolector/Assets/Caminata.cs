d9bed9c1195cd12e1399c9b02dd1a1b52d8ec5fd
alosaurio <franco.uruguayo12@gmail.com>
2020-05-01 02:49:24 -0300

    PrimerCommit

---
+++ Recolector/Assets/Caminata.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caminata : MonoBehaviour {
+   
+    public float moveSpeed = 5f;    
+    float direccionx;
+    Rigidbody2D prota;
+    bool caminandoDerecha = true;
+    bool mover = true;
+   
+    // Use this for initialization
+    void Start () {
+		prota= GetComponent<Rigidbody2D> ();
+        caminandoDerecha = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+         
+		direccionx = Input.GetAxisRaw ("Horizontal") * moveSpeed;
+        if (mover)
+        {
+            if (direccionx > 0 && !caminandoDerecha)
+            {
+                Flip();
+            }
+            else if (direccionx < 0 && caminandoDerecha)
+            {
+                Flip();
+            }
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        prota.velocity = new Vector2(direccionx, prota.velocity.y);
+    }
+    
+    void Flip()
+    {
+        caminandoDerecha = !caminandoDerecha;
+        Vector3 escala = transform.localScale;
+        escala.x = escala.x * -1;
+        transform.localScale = escala;
+    }
+} 
+    
+
+
+
+
+
+
+
+		
+	
