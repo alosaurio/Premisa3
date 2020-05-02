4b17adc7cf265033eec25b59665ff775d26b8944
alosaurio <franco.uruguayo12@gmail.com>
2020-05-01 22:42:31 -0300

    Añadiendo la generacion de objetos
    Añadí la generación de objetos en posiciones aleatorias en el eje x y su interacción con el jugador

---
+++ Recolector/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //Debe encargerse de generar los objetos y controlar el estado del juego, los puntos del jugador y actualizar el texto
+    public GameObject[] objetos;
+    public float delay;
+
+    private bool gameOver;
+    private int objetivoPerdidos;
+    private int puntos;
+    public Text puntosText;
+    
+    
+     // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        objetivoPerdidos = 0;
+        puntos = 0;
+        StartCoroutine(GeneraObjetos());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    
+    //Genera un objeto aleatorio con un intervalo entre cada uno de tres segundos mientras el juego se encuntra activo
+    IEnumerator GeneraObjetos()
+    {
+        while (gameOver==false)
+        {
+            yield return new WaitForSeconds(delay);
+            int index = Random.Range(0, objetos.Length);
+            Instantiate(objetos[index]);
+        }
+    }
+
+    public void ActualizaTexto(int puntosParaAgregar)
+    {
+        puntos += puntosParaAgregar;
+        puntosText.text = "Puntos: "+ puntos.ToString();
+    }
+    public void ObjetivoPerdidos()
+    {
+        objetivoPerdidos++;
+    }
+}
---
+++ Recolector/Assets/Scripts/Objetivos.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objetivos : MonoBehaviour
+{
+    public GameManager gameManager;
+    public int puntos;
+
+    float spawnPosicionX = 8f;
+    float spawnPosicionY = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        transform.position = PosicionRandom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //Devuelve una posicion aleatoria que se llama al crear el objeto
+    Vector3 PosicionRandom()
+    {
+        float posicionX = Random.Range(spawnPosicionX, -spawnPosicionX);
+
+        Vector3 posicionSpawn = new Vector3(posicionX, spawnPosicionY);
+        return posicionSpawn;
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            Destroy(gameObject);
+
+            if (this.gameObject.CompareTag("Pizza"))
+            {  
+                gameManager.ObjetivoPerdidos();
+            }
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            gameManager.ActualizaTexto(puntos);
+        }
+    }
+}
