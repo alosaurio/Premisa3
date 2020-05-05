6ba36b6ba988215608502ac80195715d97dc06af
Matias Master <theterrorulquiorra@gmail.com>
2020-05-04 23:29:21 -0300

    Agregado de Audio
    - Componentes AudioSource
    - Codigos de Audio en colision

---
+++ Recolector/Assets/Scripts/Audio2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio2 : MonoBehaviour
+{
+    AudioSource audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D otro)
+    {
+
+        if (otro.gameObject.tag.Equals("Asco"))
+        {
+            audiosource.Play();
+            Destroy(otro.gameObject);
+            audiosource.Play();
+
+        }
+    }
+}
